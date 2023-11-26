4f39cb5a4aeca39c2194c484fd88285a4423e1bd
BrouceM <cedrick.kamsi@gmail.com>
2023-11-26 12:34:28 +0100

    first commit

---
+++ src/test/java/com/multi/multiapp/app/MultiappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multi.multiapp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
